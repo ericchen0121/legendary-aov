cc81d70df0b76cabb1a77b48f04b3a8d41a821d0
Eric Chen <eric.chen0121@gmail.com>
2018-05-23 22:49:22 -0700

    remove sql?...

--- src/config.js
+++ src/config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
 
   // Database
-  databaseUrl: process.env.DATABASE_URL || 'sqlite:database.sqlite',
+  databaseUrl: process.env.DATABASE_URL,
 
   // Web analytics
   analytics: {
